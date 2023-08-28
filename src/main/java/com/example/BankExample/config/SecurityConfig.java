068a02088a320ef17ed789036a00f3618f8414f0
Sweing Nakarmi <sweing111@gmail.com>
2023-08-28 22:47:32 +0545

    CORS policy bean added

--- src/main/java/com/example/BankExample/config/SecurityConfig.java
+++ src/main/java/com/example/BankExample/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.BankExample.config;
 
 import com.example.BankExample.filter.JwtFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -10,6 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
@@ -28,5 +32,24 @@ public class SecurityConfig implements WebMvcConfigurer {
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public FilterRegistrationBean coresFilter(){
+        UrlBasedCorsConfigurationSource source =new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOriginPattern("*");
+        corsConfiguration.addAllowedHeader("Authorization");
+        corsConfiguration.addAllowedHeader("Content-Type");
+        corsConfiguration.addAllowedHeader("Accept");
+        corsConfiguration.addAllowedHeader("POST");
+        corsConfiguration.addAllowedHeader("GET");
+        corsConfiguration.addAllowedHeader("DELETE");
+        corsConfiguration.addAllowedHeader("PUT");
+        corsConfiguration.addAllowedHeader("OPTIONS");
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        return bean;
     }
 }
