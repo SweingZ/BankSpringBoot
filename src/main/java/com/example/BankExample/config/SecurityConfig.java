75a091d57a285c0b8640bda74bfaa6d863672982
Sweing Nakarmi <sweing111@gmail.com>
2023-08-31 00:16:58 +0545

    CORS policy fixed for login

--- src/main/java/com/example/BankExample/config/SecurityConfig.java
+++ src/main/java/com/example/BankExample/config/SecurityConfig.java
@@ -35,21 +35,13 @@ public class SecurityConfig implements WebMvcConfigurer {
     }
 
     @Bean
-    public FilterRegistrationBean coresFilter(){
-        UrlBasedCorsConfigurationSource source =new UrlBasedCorsConfigurationSource();
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.addAllowedOriginPattern("*");
-        corsConfiguration.addAllowedHeader("Authorization");
-        corsConfiguration.addAllowedHeader("Content-Type");
-        corsConfiguration.addAllowedHeader("Accept");
-        corsConfiguration.addAllowedHeader("POST");
-        corsConfiguration.addAllowedHeader("GET");
-        corsConfiguration.addAllowedHeader("DELETE");
-        corsConfiguration.addAllowedHeader("PUT");
-        corsConfiguration.addAllowedHeader("OPTIONS");
-        source.registerCorsConfiguration("/**",corsConfiguration);
-        FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter(source));
-        return bean;
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
